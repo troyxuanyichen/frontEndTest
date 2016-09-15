5d27a7f86e13519dacac416521a9e8d114336e30
Xuanyi Chen <troychen@bu.edu>
2016-09-15 15:04:30 -0400

    initialization

---
+++ app/scripts/controllers/mynewcontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoexampleApp.controller:MynewcontrollerCtrl
+ * @description
+ * # MynewcontrollerCtrl
+ * Controller of the yoexampleApp
+ */
+angular.module('yoexampleApp')
+  .controller('MynewcontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
